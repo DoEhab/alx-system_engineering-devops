89df6dada43487a4542002372b9f375b6c83e4c5
doehab <dohaehab@gmail.com>
2025-01-10 23:56:39 +0200

    task2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # This script kills a running process
 exec { 'kill_process':
-command => 'usr/bin/pkill -f killmenow',
+command => '/usr/bin/pkill -f killmenow',
+path    => '/usr/bin/:/bin/:usr/local/bin/',
 }
 
