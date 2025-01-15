2a3f714b9657995617eae041353cbcd230a9a1ac
dohaehab <dohaehab@gmail.com>
2025-01-15 22:57:50 +0200

    fix type

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # This script installs flask 
 exec { 'install_flask':
 command => '/usr/bin/pip3 install flask==2.1.0'
-path   => '/usr/bin/:/usr/local/bin/:/bin/',
+path    => ['/usr/bin/','/usr/local/bin/','/bin']
 }
 
