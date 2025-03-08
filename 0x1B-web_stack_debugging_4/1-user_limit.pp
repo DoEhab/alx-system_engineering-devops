ecd71e0a46d2a489210a484db36913571d8372e3
dohaehab <dohaehab@gmail.com>
2025-03-08 15:52:27 +0200

    task0

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,13 @@
 #update the file limit for holberton user
 exec { 'holberton-hard-limit':
-  command => 'grep -q "^holberton hard nofile" /etc/security/limits.conf || echo "holberton hard nofile 50000" >> /etc/security/limits.conf',
+  command => 'grep -q "^holberton hard nofile" /etc/security/limits.conf || \
+  echo "holberton hard nofile 50000" >> /etc/security/limits.conf',
   path    => ['/bin', '/usr/bin'],
 }
 
 #update the file limit for holberton user
 exec { 'holberton-soft-limit':
-  command => 'grep -q "^holberton soft nofile" /etc/security/limits.conf || echo "holberton soft nofile 50000" >> /etc/security/limits.conf',
+  command => 'grep -q "^holberton soft nofile" /etc/security/limits.conf || \
+  echo "holberton soft nofile 50000" >> /etc/security/limits.conf',
   path    => ['/bin', '/usr/bin'],
 }
